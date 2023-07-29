489e54ba2a2261b20d42e4ab03b15917e62d51ad
Shams M. Monem <shamsmmonem@gmail.com>
2023-07-29 23:46:27 +0300

    generated more arrays for php and made it in a loop

--- extract-font-awesome-5.php
+++ extract-font-awesome-5.php
@@ -13,41 +13,51 @@ if (!file_exists($output_dir_php)) {
     mkdir($output_dir_php, 0777, true);
 }
 
-$formatter = new SimpleFormatter(
-    $output_dir_php,
-    "solids_key_icon_value_icon_label"
-);
-$extractor = new SimpleExtractor($path, false, $formatter, 'solid', false);
-$extractor->extract();
+$iterations = [
+    0 => [
+        'output_filename' => "solids_key_icon_value_icon_label",
+        'style' => 'solid',
+        'mode' => SimpleFormatter::MODE_KEY_ICON_VALUE_LABEL,
+    ],
+    1 => [
+        'output_filename' => "brands_key_icon_value_icon_label",
+        'style' => 'brands',
+        'mode' => SimpleFormatter::MODE_KEY_ICON_VALUE_LABEL,
+    ],
+    2 => [
+        'output_filename' => "regulars_key_icon_value_icon_label",
+        'style' => 'regular',
+        'mode' => SimpleFormatter::MODE_KEY_ICON_VALUE_LABEL,
+    ],
+    3 => [
+        'output_filename' => "solids_just_an_array",
+        'style' => 'solid',
+        'mode' => SimpleFormatter::MODE_JUST_AN_ARRAY,
+    ],
+    4 => [
+        'output_filename' => "brands_just_an_array",
+        'style' => 'brands',
+        'mode' => SimpleFormatter::MODE_JUST_AN_ARRAY,
+    ],
+    5 => [
+        'output_filename' => "regulars_just_an_array",
+        'style' => 'regular',
+        'mode' => SimpleFormatter::MODE_JUST_AN_ARRAY,
+    ],
+];
 
-try {
-    $extractor->getFormatter()->saveFile();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+foreach ($iterations as $iteration) {
+    $formatter = new SimpleFormatter(
+        $output_dir_php,
+        $iteration['output_filename'],
+        $iteration['mode']
+    );
+    $extractor = new SimpleExtractor($path, false, $formatter, $iteration['style'], false);
+    $extractor->extract();
 
-$formatter = new SimpleFormatter(
-    $output_dir_php,
-    "brands_key_icon_value_icon_label"
-);
-$extractor = new SimpleExtractor($path, false, $formatter, 'brands', false);
-$extractor->extract();
-
-try {
-    $extractor->getFormatter()->saveFile();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
-
-$formatter = new SimpleFormatter(
-    $output_dir_php,
-    "regulars_key_icon_value_icon_label"
-);
-$extractor = new SimpleExtractor($path, false, $formatter, 'regular', false);
-$extractor->extract();
-
-try {
-    $extractor->getFormatter()->saveFile();
-} catch (Exception $e) {
-    die($e->getMessage());
+    try {
+        $extractor->getFormatter()->saveFile();
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
 }
